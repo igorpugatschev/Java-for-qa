82df6b07520bbbcb7afbcd0751830da52b6011b7
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-03-06 14:55:45 +0700

    работа с функциями и классами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++
@@ -1,8 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class MyFirstProgramm {
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
-
-}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramDemo.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class myFirstProgramDemo {
+	public static void main(String[] args){
+		String somebody = "world";
+		System.out.println("Hello, " + somebody + "!");
+
+		double l = 8;
+		double s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
+	}
+
+}
