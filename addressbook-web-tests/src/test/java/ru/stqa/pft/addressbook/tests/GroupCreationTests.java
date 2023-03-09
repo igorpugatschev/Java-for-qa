54c35aad8674b3caa5852ed0190e9b8ec131759b
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-03-09 14:24:08 +0700

    Refactor tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,53 +10,79 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GroupCreationTests {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-    driver = new FirefoxDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-    driver.get("http://127.0.0.1/addressbook/");
-    driver.manage().window().setSize(new Dimension(855, 952));
-    driver.findElement(By.xpath("//input[@name=\'user\']")).click();
-    driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys("admin");
-    driver.findElement(By.xpath("//input[@name=\'user\']")).click();
-    driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value=\'Login\']")).click();  }
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
 
-  @Test
-  public void testGroupCreation() {
-
-    driver.findElement(By.xpath("//a[contains(text(),\'groups\')]")).click();
-    driver.findElement(By.xpath("//input[@name=\'new\']")).click();
-    driver.findElement(By.xpath("//input[@name=\'group_name\']")).click();
-    driver.findElement(By.xpath("//input[@name=\'group_name\']")).sendKeys("group 1 test");
-    driver.findElement(By.xpath("//textarea[@name=\'group_header\']")).click();
-    driver.findElement(By.xpath("//textarea[@name=\'group_header\']")).sendKeys("header");
-    driver.findElement(By.xpath("//textarea[@name=\'group_footer\']")).click();
-    driver.findElement(By.xpath("//textarea[@name=\'group_footer\']")).sendKeys("footer");
-    driver.findElement(By.xpath("//input[@name=\'submit\']")).click();
-    driver.findElement(By.xpath("//a[contains(text(),\'group page\')]")).click();
-    driver.findElement(By.xpath("//a[contains(text(),\'home\')]")).click();
-    driver.findElement(By.xpath("//a[contains(text(),\'Logout\')]")).click();
-    driver.close();
-  }
-
-
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-
-  public static boolean isAlertPresent(FirefoxDriver driver) {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+        driver.get("http://127.0.0.1/addressbook/");
+        driver.manage().window().setSize(new Dimension(855, 952));
+        login("admin", "secret");
     }
-  }
+
+    private void login(String username, String password) {
+
+        driver.findElement(By.xpath("//input[@name=\'user\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'user\']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@name=\'user\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'pass\']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+    }
+
+    @Test
+    public void testGroupCreation() {
+
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm("group 1", "header of group 1", "footer of group 1");
+        submitGroupCreation();
+        returnToGroupPage();
+
+    }
+
+    private void returnToGroupPage() {
+        driver.findElement(By.xpath("//a[contains(text(),\'group page\')]")).click();
+    }
+
+    private void submitGroupCreation() {
+        driver.findElement(By.xpath("//input[@name=\'submit\']")).click();
+    }
+
+    private void fillGroupForm(String name, String header, String footer) {
+        driver.findElement(By.xpath("//input[@name=\'group_name\']")).click();
+        driver.findElement(By.xpath("//input[@name=\'group_name\']")).sendKeys(name);
+        driver.findElement(By.xpath("//textarea[@name=\'group_header\']")).click();
+        driver.findElement(By.xpath("//textarea[@name=\'group_header\']")).sendKeys(header);
+        driver.findElement(By.xpath("//textarea[@name=\'group_footer\']")).click();
+        driver.findElement(By.xpath("//textarea[@name=\'group_footer\']")).sendKeys(footer);
+    }
+
+    private void initGroupCreation() {
+        driver.findElement(By.xpath("//input[@name=\'new\']")).click();
+    }
+
+    private void gotoGroupPage() {
+        driver.findElement(By.xpath("//a[contains(text(),\'groups\')]")).click();
+    }
+
+
+    @After
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver driver) {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
 }
