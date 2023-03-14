a7bed6922dc4f9f64f207496af4bae4cb47949ba
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-03-14 17:49:50 +0700

    update project

--- LearnAutomation/src/test/java/SearchTest.java
+++ LearnAutomation/src/test/java/SearchTest.java
@@ -1,9 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class SearchTest {
 
@@ -11,14 +14,27 @@ public class SearchTest {
     public void openGoogleComInChromeTest() {
         File file = new File("src/test/resources/geckodriver.exe");//подключаем драйвер браузера
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());//передаём путь к драйверу в системные переменные
-        //System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+
         WebDriver driver = new FirefoxDriver();//создаём объект драйвер
         //используем FireFox
         //Google Chrome - не запускается
         driver.navigate().to("https://www.google.com/");//переход по указанной ссылке
         //driver.get("https://www.google.com/");//то же, что driver.navigate().to()
-        driver.navigate().refresh();//обновить текущую страницу
         System.out.println(driver.getTitle());//выводим в консоль заголовок страницы
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.click();
+
+        driver.navigate().refresh();//обновить текущую страницу
+
+        WebElement googleAppsButton = driver.findElement(By.cssSelector("div#gbwa a"));
+        googleAppsButton.click();
+
+        driver.navigate().refresh();//обновить текущую страницу
+        List<WebElement> googleSubmitButtons = driver.findElements(By.xpath("//input[@name='btnI']"));
+        googleSubmitButtons.get(1).click();
+
         driver.quit();//выйти и закрыть все окна
 
     }
