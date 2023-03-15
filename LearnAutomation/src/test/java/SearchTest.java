41387cfa73d512db5a12fe2e14a84b41cecb24fb
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-03-15 16:11:03 +0700

    Assertions
    Добавляем тестовые проверки.
    Библиотека assertJ

--- LearnAutomation/src/test/java/SearchTest.java
+++ LearnAutomation/src/test/java/SearchTest.java
@@ -2,17 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
-import java.util.List;
+
 
 public class SearchTest {
 
     @Test
-    public void openGoogleComInChromeTest() throws InterruptedException {
+    public void openGoogleComInFireFoxTest() throws InterruptedException {
         File file = new File("src/test/resources/geckodriver.exe");//подключаем драйвер браузера
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());//передаём путь к драйверу в системные переменные
 
@@ -26,15 +26,24 @@ public class SearchTest {
 
         //Scenario with Search 'selenium java' & Enter button
 
-        WebElement searchField = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+        WebElement searchField = driver.findElement(By.xpath("//input[@name='q']"));
         searchField.click();
         searchField.sendKeys("selenium java");
         searchField.sendKeys(Keys.ENTER);
         Thread.sleep(3000);
 
-        WebElement resultRow = driver.findElement(By.xpath("/html/body/div[7]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3"));
-        System.out.println(resultRow.getText());
-        System.out.println(resultRow.getAttribute("class"));
+        WebElement resultRow = driver.findElement(By.xpath("//div[@class='g']//h3"));
+        assertThat(resultRow.isDisplayed()).as("Element has not been displayed!").isTrue();
+        assertThat(resultRow.getText()).as("Wrong text has been displayed!").isEqualTo("The Selenium Browser Automation Project");
+        assertThat(resultRow.getAttribute("class")).as("Wrong attribute text").contains("LC20lb MBeuO DKV0Md");
+
+/**
+        assertTrue(resultRow.isDisplayed(), "Element has not been displayed!");
+        assertEquals(resultRow.getText(), "The Selenium Browser Automation Project", "Wrong text has been displayed!");
+        assertEquals(resultRow.getAttribute("class"), "LC20lb MBeuO DKV0Md", "Wrong attribute text");
+         */
+
+
 
 
 
