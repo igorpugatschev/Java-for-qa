09ec9efc9a83a538c8c0ccdbfa0cdd9f06107db3
igor.pugatschev <igor.pugatschev@yandex.ru>
2023-03-24 16:23:37 +0700

    Извлечение body headers cookie и других данных из ответа с помощью extract

--- apiTesting/src/test/java/FirstTest.java
+++ apiTesting/src/test/java/FirstTest.java
@@ -1,5 +1,9 @@
 import config.TestConfig;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.Map;
 
 import static constants.Constants.Actions.SWAPI_GET_PEOPLE;
 import static constants.Constants.Path.SWAPI_PATH;
@@ -29,5 +33,37 @@ public class FirstTest extends TestConfig {
                 .body("count",equalTo(82))
                 .body("results.name[0]",equalTo("Luke Skywalker"))
                 .log().body();
+    }
+
+    @Test
+    public void getAllDataFromRequest(){
+        Response response =
+                given().spec(requestSpecificationForSwapiTests).log().uri()
+                        .when().get(SWAPI_PATH)
+                        .then().extract().response();
+        String jsonResponseAsString = response.asString();;
+        System.out.println(jsonResponseAsString);
+    }
+
+    @Test
+    public void getCookieFromResponse(){
+        Response response =
+                given().spec(requestSpecificationForSwapiTests).log().uri()
+                        .when().get(SWAPI_PATH)
+                        .then().extract().response();
+        Map<String,String> allCookie = response.getCookies();
+        System.out.println("allCookie--> " + allCookie);
+    }
+
+    @Test
+    public void getHeadersFromResponse() {
+        Response response =
+                given().spec(requestSpecificationForSwapiTests).log().uri()
+                        .when().get(SWAPI_PATH)
+                        .then().extract().response();
+        Headers headers = response.getHeaders();
+        System.out.println("headers--> " + headers);
+        String contentType = response.getContentType();
+        System.out.println("contenetType--> "+ contentType);
     }
 }
